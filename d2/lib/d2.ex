491d12194e2b8cce31d901949464fea7aaa84239
carn181 <carn181@proton.me>
2025-12-02 22:19:30 -0500

    Day 2: Async Brute Force

--- d2/lib/d2.ex
+++ d2/lib/d2.ex
@@ -23,50 +23,43 @@ defmodule D2 do
   """
   def solve1 do
     "input.txt"
-    |> File.read!()
-    |> String.split()
-    |> hd
-    |> String.split(",")
-    |> Enum.map(fn x -> String.split(x, "-") end)
-    |> find_invalid(&twice/1)
+    |> process_ranges()
+    |> count_invalid(&twice/1)
   end
 
   def solve2 do
     "input.txt"
-    |> File.read!()
-    |> String.split()
-    |> hd
-    |> String.split(",")
-    |> Enum.map(fn x -> String.split(x, "-") end)
-    |> find_invalid(&atleasttwice/1)
+    |> process_ranges()
+    |> count_invalid(&atleasttwice/1)
   end
 
-  def find_invalid(ranges, invalid_checker) do
-    Enum.reduce(
-      ranges,
-      0,
-      fn range, sum ->
-        [left, right] = Enum.map(range, &String.to_integer/1)
+  def count_invalid(ranges, checker) do
+    ranges
+    |> Task.async_stream(fn [left, right] ->
+      left..right
+      |> Enum.filter(fn num ->
+        num
+        |> Integer.to_string()
+        |> checker.()
+      end)
+      |> Enum.sum()
+    end)
+    |> Enum.reduce(0, fn {:ok, sum}, acc -> acc + sum end)
+  end
 
-        curr =
-          Enum.reduce(left..right, 0, fn i, total ->
-            str = Integer.to_string(i)
-
-            if invalid_checker.(str) do
-              total + i
-            else
-              total
-            end
-          end)
-
-        sum + curr
-      end
-    )
+  def process_ranges(file) do
+    file
+    |> File.read!()
+    |> String.trim()
+    |> String.split(",")
+    |> Enum.map(fn str ->
+      Enum.map(String.split(str, "-"), &String.to_integer/1)
+    end)
   end
 
   defp twice(str) do
     n = String.length(str)
-    rem(n,2) == 0 && String.slice(str, 0, div(n, 2)) == String.slice(str, div(n, 2), n)
+    rem(n, 2) == 0 && String.slice(str, 0, div(n, 2)) == String.slice(str, div(n, 2), n)
   end
 
   def all_equal?(list) do
@@ -83,12 +76,13 @@ defmodule D2 do
   def factors(1) do
     []
   end
+
   def factors(n) when n > 1 do
     # Iterate from 1 up to n and check for divisibility
-    1..n-1
+    1..(n - 1)
     |> Enum.filter(fn i -> rem(n, i) == 0 end)
   end
-  
+
   def atleasttwice(str) do
     str
     |> String.length()
